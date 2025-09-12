dd6032dff1df9ca0dd16c8b97761ffd238372428
Rene Moeijes <simpelmuis@gmail.com>
2025-09-12 13:48:08 +0200

    Initial commit

--- PushToGit.sh
+++ PushToGit.sh
@@ -1,11 +1,9 @@
-#ssh-keygen -t ed25519 -C "rene.moeijes@kpnmail.nl"
 cat ~/.ssh/id_ed25519.pub
-git remote set-url origin git@github.com:renemoeijes/wifi-config-ap.git
 git config --global user.email "simpelmuis@gmail.com"
 git config --global user.name "Rene Moeijes"
 git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/renemoeijes/wifi-config-ap.git
-git push -u origin main
+git remote add origin git@github.com:renemoeijes/wifi-config-ap.git
+git push --force -u origin main
